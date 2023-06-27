7597c4a6995955abe6825a44e8edb09cac27fb52
i.mikhalov <112470475+larbadge@users.noreply.github.com>
2023-06-27 18:40:16 +0300

    check upped code

--- src/main/java/com/devcom/puzzles/config/AppConfig.java
+++ src/main/java/com/devcom/puzzles/config/AppConfig.java
@@ -1,10 +1,12 @@
 package com.devcom.puzzles.config;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AppConfig {
-
     static {
         nu.pattern.OpenCV.loadLocally();
     }
