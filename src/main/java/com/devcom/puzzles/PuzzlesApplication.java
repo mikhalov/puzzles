421e52807c295ee583a5634e30b62a3cabf9a0a9
i.mikhalov <mihalovivan90@gmail.com>
2024-04-18 23:00:26 +0300

    commit

--- src/main/java/com/devcom/puzzles/PuzzlesApplication.java
+++ src/main/java/com/devcom/puzzles/PuzzlesApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class PuzzlesApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(PuzzlesApplication.class, args);
     }
